e6ef6e8df76119f57acd836d23e772b2cd6c6353
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2022-12-16 21:14:18 -0600

    Master

--- src/main/kotlin/me/deotime/Storage.kt
+++ src/main/kotlin/me/deotime/Storage.kt
@@ -1,28 +1,42 @@
 package me.deotime
 
+import co.q64.raindrop.Empty
 import co.q64.raindrop.optics.Getter
 import co.q64.raindrop.optics.Lens
+import co.q64.raindrop.standard.algebra.orEmpty
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flow
-
+import kotlin.reflect.KProperty
 
 interface Storage {
 
     val root: String
     val name: String
 
-    interface Property<T> {
+    interface Member {
+
+        val name: String
+        val storage: Storage
+
+        operator fun getValue(ref: Any?, prop: KProperty<*>) = this
+
+        interface Delegate<T : Member> {
+            operator fun provideDelegate(ref: Storage, prop: KProperty<*>): T
+        }
+
+    }
+
+    interface Property<T> : Member {
+
         suspend fun get(): T
         suspend infix fun <U> get(getter: Getter<T, U>) = getter.view(get())
 
         suspend fun <U> set(setter: Lens.Simple<T, U>, value: U) = update(setter) { value }
         suspend infix fun set(value: T)
+
     }
 
 
-    interface Collection<T> {
+    interface Collection<T> : Member {
 
         val size: Int
         fun toFlow(): Flow<T>
@@ -45,9 +59,8 @@ interface Storage {
 
 }
 
-fun test() {
-    listOf<String>().asFlow()
-}
+inline fun <reified T> Storage.property(): Storage.Member.Delegate<Storage.Property<T?>> = property(null)
+inline fun <reified T> Storage.property(default: T): Storage.Member.Delegate<Storage.Property<T>> = TODO()
 
 suspend inline fun <T, U> Storage.Property<T>.update(lens: Lens.Simple<T, U>, closure: (U) -> U): Unit =
     get().let { Storage.Update(it, lens.set(it, lens.view(it).let(closure)).also { set(it) }) }
